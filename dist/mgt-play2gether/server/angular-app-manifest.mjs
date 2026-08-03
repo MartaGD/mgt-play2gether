5a81d5818bb9cad6ce1c2f9db6af901b03b277a1
Marta G <marta.483@hotmail.com>
2026-08-04 01:13:06 +0200

    change grid

--- dist/mgt-play2gether/server/angular-app-manifest.mjs
+++ dist/mgt-play2gether/server/angular-app-manifest.mjs
@@ -37,8 +37,8 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 1598, hash: '3ecd773d392795907be8979f77b09ff0393d56adea8faf4beb9aa0bab2e87424', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1164, hash: 'f47006e67c0ec8b42071fe9fcdc8a058d9f273d358ae6ab816ea54d1ba2d9821', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 1598, hash: 'b12264bb9c7b856787dda5ca33fcb6ad51840ed0d68bc2967b1f041cff958ee0', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1164, hash: '9542af96cec13ba3b4799b7660abf61a2d1aadff53e10703233771e170f09241', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-CAEAGL2V.css': {size: 133262, hash: 'gne5NF85Ml0', text: () => import('./assets-chunks/styles-CAEAGL2V_css.mjs').then(m => m.default)}
   },
 };
