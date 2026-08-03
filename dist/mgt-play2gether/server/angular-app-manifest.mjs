4a0f54c1e04a16b2d7d1cdc04b29570506a13d50
Marta G <marta.483@hotmail.com>
2026-08-04 00:38:17 +0200

    reupload dist files after fixing the start game button disabled condition

--- dist/mgt-play2gether/server/angular-app-manifest.mjs
+++ dist/mgt-play2gether/server/angular-app-manifest.mjs
@@ -15,6 +15,10 @@ export default {
   },
   {
     "renderMode": 0,
+    "route": "/kingdom"
+  },
+  {
+    "renderMode": 0,
     "route": "/treachery"
   },
   {
@@ -33,8 +37,8 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 1445, hash: '556e35db70d63562bb00708c9b9aed2a9166d0d68e6aa201755053d7d24b3cc8', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1011, hash: '5b94e6935509326cbad6628091351e6679f434899bbb134dad9e1cee7c39e119', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 1445, hash: '90294fd8b41ba3833eb6f59ce8d538942c597c786c86f012caaeb1c6bcfda066', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1011, hash: 'b6b409c425f09b53f2f3a0287f68b26056494489046d1efd4e9bde433accc1a2', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-CAEAGL2V.css': {size: 133262, hash: 'gne5NF85Ml0', text: () => import('./assets-chunks/styles-CAEAGL2V_css.mjs').then(m => m.default)}
   },
 };
